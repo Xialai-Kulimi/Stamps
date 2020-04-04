7cede7a4ae6a675b35694eb90d7ee9ec20a40637
SilkooDarze <tsaotsaotsao51510@gmail.com>
2020-04-04 15:58:19 +0800

    fix

--- new/now_best_check_way_2.py
+++ new/now_best_check_way_2.py
@@ -1,7 +1,7 @@
 import math
 import time
 
-n = 8
+n = 4
 
 while True:
 
@@ -11,8 +11,9 @@ while True:
 
     k = n
     k_list = []
+    k_dict = {}
 
-    check_times = int((len(stamps) - (len(stamps) % 2))/2)
+    check_times = int((len(stamps) - (len(stamps) % 2)) / 2)
 
     for i in range(int(math.pow(p, n))):
 
@@ -32,11 +33,11 @@ while True:
             continue  # no 1 in stamps
 
         for j in range(check_times):
-            if stamps[j] > stamps[n-j-1]:
+            if stamps[j] > stamps[n - j - 1]:
                 break
         else:
             record = []
-
+            # print(stamps, i)
             for j in range(n):  # Methods of coloring.
                 for r in range(j, n):
                     summary = 0
@@ -51,10 +52,14 @@ while True:
                 x = record[j]
                 if x + 1 < record[j + 1]:
                     if x > k:
-                        k_list = [i]
+                        k_list = [str(i) + ':' + str(stamps)]
+                        # k_dict = {i: str(stamps)}
                         k = x
                     elif x == k:
-                        k_list.append(i)
+                        k_list.append(str(i) + ':' + str(stamps))
+                        # k_dict[i] = str(stamps)
+                        # print(stamps)
+                        # print(k_list)
                     break
             else:
                 print('error occur')
@@ -63,6 +68,7 @@ while True:
 
     print(n)
     print(k, k_list)
+    # print(k_dict)
     f = open('result.md', 'a')
-    f.write(f'n: {n}\nk: {k} method: {k_list}')
+    f.write(f'n: {n}\nk: {k} method: {k_list}\n')
     n += 1

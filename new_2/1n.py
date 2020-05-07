7c9cca30123acc59564284096d3a0defe232fcde
Kulimi <tsaotsaotsao51510@gmail.com>
2020-05-07 16:07:35 +0800

    upgrade

--- new_2/1n.py
+++ new_2/1n.py
@@ -1,1 +1,78 @@
+import math
+import time
 
+n = 5
+
+while True:
+    start_time = time.time()
+    p = n * (n + 1) // 2
+
+    stamps = [1] * n
+
+    k = n
+    k_list = []
+    # k_dict = {}
+
+    check_times = (len(stamps) - (len(stamps) % 2)) // 2
+
+    for i in range(int(math.pow(p, n))):
+
+        num_pos = 0
+        for j in stamps:  # 下一種面值分配
+            print(stamps)
+            stamps[num_pos] += 1
+            if stamps[num_pos] == p + 1:
+                stamps[num_pos] = 1
+                num_pos += 1
+            else:
+                break
+
+        # if stamps[0] == 1:
+        #     break
+
+        # for j in stamps:  # must be one 1 stamp
+        #     if j == 1:
+        #         break
+        # else:
+        #     continue  # no 1 in stamps
+
+        for j in range(check_times):
+            if stamps[j] > stamps[n - j - 1]:
+                break  # Skip this time
+            elif stamps[j] == stamps[n - j - 1]:
+                continue
+            else:
+                record = []
+                # print(stamps, i)
+                for m in range(n):  # Methods of coloring.
+                    for r in range(m, n):
+                        summary = 0
+                        for q in range(m, r + 1):
+                            summary += stamps[q]
+                        record.append(summary)
+
+                record.append(p + 1)  # Make a BIG gap.
+                record.sort()
+
+                for m in range(len(record)):
+                    x = record[m]
+                    if x + 1 < record[m + 1]:
+                        if x > k:
+                            k_list = [str(i) + ':' + str(stamps)]
+                            k = x
+                        elif x == k:
+                            k_list.append(str(i) + ':' + str(stamps))
+                        break
+                else:
+                    print('error occur')
+                    print(record)
+                    input()
+                break
+
+    print(n)
+    print(k, k_list)
+    f = open('result.md', 'a')
+    f.write(f'n: {n}\nk: {k} method: {k_list}\n')
+    f.close()
+    n += 1
+    print(time.time() - start_time)
--- old/new/test.py
+++ old/new/test.py
@@ -1,34 +1,37 @@
-from multiprocessing import Process, Queue
-import os, time, random
+import time
+import math
 
+b = 0
+a = int(4294967296*4294967296)
 
-# 寫資料程序執行的程式碼:
-def write(q):
-    print('Process to write: {}'.format(os.getpid()))
-    for value in ['A', 'B', 'C']:
-        print('Put %s to queue...' % value)
-        q.put(value)
-        time.sleep(random.random())
+start_time = time.time()
 
+while a != 1:
+    a = math.pow(a, 0.5)
+    b += 1
 
-# 讀資料程序執行的程式碼:
-def read(q):
-    print('Process to read:{}'.format(os.getpid()))
-    while True:
-        value = q.get()
-        print('Get %s from queue.' % value)
+print(time.time() - start_time, b)
 
+b = 0
 
-if __name__ == '__main__':
-    # 父程序建立Queue，並傳給各個子程序：
-    q = Queue()
-    pw = Process(target=write, args=(q,))
-    pr = Process(target=read, args=(q,))
-    # 啟動子程序pw，寫入:
-    pw.start()
-    # 啟動子程序pr，讀取:
-    pr.start()
-    # 等待pw結束:
-    pw.join()
-    # pr程序裡是死迴圈，無法等待其結束，只能強行終止:
-    pr.terminate()
+a = int(4294967296*4294967296)
+
+start_time = time.time()
+
+while a != 1:
+    a = pow(a, 0.5)
+    b += 1
+
+print(time.time() - start_time, b)
+
+b = 0
+
+a = int(4294967296*4294967296)
+
+start_time = time.time()
+
+while a != 1:
+    a = a ** 0.5
+    b += 1
+
+print(time.time() - start_time, b)

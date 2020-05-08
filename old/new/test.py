ae25d385bd39118288369210e9689cc58fd45da5
Kulimi <tsaotsaotsao51510@gmail.com>
2020-05-08 13:03:52 +0800

    Update test.py

--- old/new/test.py
+++ old/new/test.py
@@ -1,6 +1,37 @@
-a = 'ifmmp!npuifs!gvdifs'
 
-a = list(a)
 
-for letter in a:
-    print(chr(ord(letter)-1), end='')
+a = [[1, 12, 34], [24, 43, 52]]
+b = []
+c = [1, [1, 2, 3], [1, [1, 2, 1, [1,[1], 2], 1]]]
+
+list_list = []
+
+for sub_list in c:
+    x = 0
+    while True:
+
+        try:
+            sub_list = sub_list[0]
+            x += 1
+        except:
+            break
+    print(sub_list)
+    list_list.append(x)
+
+list_list.sort()
+print(c)
+print(list_list[len(list_list)-1])
+
+
+
+
+'''
+1  12 34
+34 32 34
+
+-1 
+
+1 12 34 -1 34 32 34
+'''
+
+
